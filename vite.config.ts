d1d853e2bb5735095dfb6c97a4f4bfefad19b346
Sabriel <sadewobrilianrafael@gmail.com>
2026-03-05 10:44:38 +0700

    Modify gitignore and viteconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            buildDirectory: 'public/build',
             refresh: true,
         }),
         tailwindcss(),
