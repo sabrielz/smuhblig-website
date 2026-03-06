608ae16d6acfe719778ddd3b35fa36fb36088481
Sabriel <sadewobrilianrafael@gmail.com>
2026-03-06 21:29:54 +0700

    fix: Wrong build directory and rebuild assets.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            buildDirectory: 'public/build',
+            buildDirectory: 'build',
             refresh: true,
         }),
         tailwindcss(),
